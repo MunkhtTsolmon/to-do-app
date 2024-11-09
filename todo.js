ef9978b7866b0c1d87317596a5a28a02b517c894
MunkhtTsolmon <monkhtsolmonmuzo@gmail.com>
2024-11-09 14:30:15 +0800

    up

--- todo.js
+++ todo.js
@@ -1,43 +1,25 @@
+const taskName = document.getElementById("taskName");
+const statusSelect = document.getElementById("statusSelect");
+const taskPopup = document.getElementById("taskPopup");
+const taskContainerMain = document.getElementById("taskContainerMain");
+function popup() {
+  taskPopup.style.display = "block";
+}
+function closePopup() {
+  taskPopup.style.display = "none";
+}
+function addTask() {
+  if (taskName.value === "") {
+    alert("You must write your task");
+  } else {
+    let taskContainer = document.createElement("div");
 
-//status update
-function statusUpdate(index, status) {
-  let item = todos[index];
-  item.status = status;
-  console.log("update", todos);
-}
-//change name
-function changeName(index, name) {
-  let newName = todos[index];
-  newName.name = name;
-  console.log("nameChange", todos);
-}
-// delete one item
-// function deleteOne(index) {
-//   todos.splice(index, 1);
-//   console.log("delete", todos);
-// }
-// function deleteOne(index) {
-//   let newTodos = todos;
-//   for (let i = 0; i < newTodos.length; i++) {
-//     if (newTodos[index] !== newTodos[i]) {
-//       newTodos.push(newTodos[i]);
-//     }
-//   }
-//   console.log(newTodos);
-// }
-function deleteOne(index) {
-  let newTodos = [];
-  for (let i = 0; i < todos.length; i++) {
-    if (todos[index] !== todos[i]) {
-      newTodos.push(todos[i]);
-    }
+    let h3Name = document.createElement("h3");
+    h3Name.innerHTML = taskName.value;
+    taskContainer.innerHTML(h3Name);
+    let h3Status = document.createElement("h3");
+    h3Status.innerHTML = statusSelect.value;
+    taskContainer.innerHTML(h3Status);
+    taskContainerMain.appendChild = taskContainer;
   }
-  console.log(newTodos);
 }
-// // running application
-// addOne({ name: "Hool hiih", status: "Done" });
-// addOne({ name: "Usand oroh", status: "to do" });
-// addOne({ name: "JS sudlah", status: "to do" });
-// statusUpdate(1, "Done");
-// changeName(1, "Untah");
-// deleteOne(2);
